1139bbf5ec60befa8463edcdb251af28b6eacb55
Dolan Miu <dolan_miu@hotmail.com>
2016-07-10 01:11:51 +0100

    initial set up code

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var Platform = require('platform');
+
+var Accessory, Service, Characteristic, UUIDGen;
+
+module.exports = function(homebridge) {
+    console.log("homebridge API version: " + homebridge.version);
+
+    // Accessory must be created from PlatformAccessory Constructor
+    Accessory = homebridge.platformAccessory;
+
+    // Service and Characteristic are from hap-nodejs
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    UUIDGen = homebridge.hap.uuid;
+
+    // For platform plugin to be considered as dynamic platform plugin,
+    // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
+    homebridge.registerPlatform("homebridge-chickencoop", "ChickenCoop", Platform, true);
+}
